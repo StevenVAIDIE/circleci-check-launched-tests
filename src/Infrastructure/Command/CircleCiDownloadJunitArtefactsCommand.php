cb70845c3847d16f37d93c0f64781b14813fd701
Steven VAIDIE <steven.vaidie@gmail.com>
2024-04-03 13:26:56 +0200

    Use Guzzle

--- src/Infrastructure/Command/CircleCiDownloadJunitArtefactsCommand.php
+++ src/Infrastructure/Command/CircleCiDownloadJunitArtefactsCommand.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace Akeneo\CircleCiDashboard\Infrastructure\Command;
 
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Pool;
+use Psr\Http\Message\ResponseInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -14,7 +19,7 @@ class CircleCiDownloadJunitArtefactsCommand extends Command
     protected static $defaultName = 'circleci:download_junit_artefacts';
     protected static $defaultDescription = 'Download all phpunit results from circle ci workflow id';
 
-    public function __construct(private string $token)
+    public function __construct(private readonly ClientInterface $circleCiClient)
     {
         parent::__construct();
     }
@@ -23,7 +28,7 @@ class CircleCiDownloadJunitArtefactsCommand extends Command
     {
         $this
             ->addArgument('workflow_id', InputArgument::REQUIRED, 'Circleci workflow id')
-            ->addArgument('output_directory', InputArgument::REQUIRED, 'Output directory')
+            ->addArgument('output_directory', InputArgument::OPTIONAL, 'Output directory', 'output/artefacts')
         ;
     }
 
@@ -33,9 +38,16 @@ class CircleCiDownloadJunitArtefactsCommand extends Command
         $outputDirectory = $input->getArgument('output_directory');
 
         $this->clearOutputDir($outputDirectory);
-        $jobs = $this->getWorkflowJobs($workflowId);
-        $junitArtefacts = $this->getJunitArtefacts($jobs);
-        $this->downloadArtefactsByChunks($junitArtefacts, $outputDirectory);
+        try {
+            $jobs = $this->getWorkflowJobs($workflowId);
+            $junitArtefacts = $this->getJunitArtefactsForJobs($jobs);
+            $this->downloadArtefacts($junitArtefacts, $outputDirectory);
+        } catch (\Throwable $e) {
+            $output->writeln("Unable to download junit artefacts");
+            $output->writeln($e->getMessage());
+
+            return Command::FAILURE;
+        }
 
         return Command::SUCCESS;
     }
@@ -62,62 +74,18 @@ class CircleCiDownloadJunitArtefactsCommand extends Command
 
     /**
      * @return string[]
-     */
-    private function getJunitArtefacts(array $jobs): array
-    {
-        $artefacts = [];
-        foreach ($jobs as $job) {
-            $curl = curl_init();
-            $projectSlug = $job['project_slug'];
-            $jobId = $job['job_number'];
-
-            curl_setopt_array($curl, array(
-                CURLOPT_URL => "https://circleci.com/api/v1.1/project/$projectSlug/$jobId/artifacts",
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_CUSTOMREQUEST => 'GET',
-                CURLOPT_HTTPHEADER => array(
-                    'Content-Type: application/json',
-                    "Circle-Token: $this->token",
-                ),
-            ));
-
-            $response = curl_exec($curl);
-            $decodedResponse = json_decode($response, true);
-
-            $artefacts = array_merge(
-                $artefacts,
-                array_map(
-                    static fn(array $artefact) => ['path' => $artefact['path'], 'url' => $artefact['url']],
-                    $decodedResponse
-                )
-            );
-        }
-
-        return array_filter($artefacts, static fn (array $artefact) => str_ends_with($artefact['path'], '.xml'));
-    }
-
-    /**
-     * @return string[]
+     * @throws GuzzleException
+     * @throws \Exception
      */
     private function getWorkflowJobs(string $workflowId): array
     {
-        $curl = curl_init();
+        $response = $this->circleCiClient->request('GET', "api/v2/workflow/$workflowId/job", [
+            'headers' => [
+                'Content-Type: application/json',
+            ]
+        ]);
 
-        curl_setopt_array($curl, array(
-          CURLOPT_URL => "https://circleci.com/api/v2/workflow/$workflowId/job",
-          CURLOPT_RETURNTRANSFER => true,
-          CURLOPT_FOLLOWLOCATION => true,
-          CURLOPT_CUSTOMREQUEST => 'GET',
-          CURLOPT_HTTPHEADER => array(
-            'Content-Type: application/json',
-            "Circle-Token: $this->token",
-          ),
-        ));
-
-        $response = curl_exec($curl);
-        $decodedResponse = json_decode($response, true);
-
+        $decodedResponse = json_decode($response->getBody()->getContents(), true);
         if ($decodedResponse['next_page_token'] !== null) {
             throw new \Exception("Unhandled next page token");
         }
@@ -128,68 +96,66 @@ class CircleCiDownloadJunitArtefactsCommand extends Command
         );
     }
 
-    private function downloadArtefactsByChunks(array $artefacts, string $outputDirectory): void
+    /**
+     * @return string[]
+     * @throws GuzzleException
+     */
+    private function getJunitArtefactsForJobs(array $jobs): array
     {
-        var_dump(count($artefacts) . " artefacts will be downloaded");
-        $artefactBatches = array_chunk($artefacts, 20);
-        foreach ($artefactBatches as $i => $artefacts) {
-            var_dump(sprintf("Download artefacts n°%d/%d", $i, count($artefactBatches)));
+        $artefacts = [];
+        foreach ($jobs as $job) {
+            $projectSlug = $job['project_slug'];
+            $jobId = $job['job_number'];
 
-            $this->downloadArtefacts($artefacts, $outputDirectory);
+            $response = $this->circleCiClient->request('GET', "api/v1.1/project/$projectSlug/$jobId/artifacts", [
+                'headers' => [
+                    'Content-Type: application/json',
+                ]
+            ]);
+
+            $decodedArtefacts = json_decode($response->getBody()->getContents(), true);
+            $junitArtefacts = array_filter(
+                $decodedArtefacts,
+                static fn (array $artefact) => str_ends_with($artefact['path'], '.xml')
+            );
+
+            $artefacts = array_merge($artefacts, array_map(
+                static fn (array $artefact) => [
+                    'filename' => sprintf('%s-%s-%s', $jobId, $artefact['node_index'], basename($artefact['path'])),
+                    'url' => $artefact['url']
+                ],
+                $junitArtefacts
+            ));
         }
+
+        return $artefacts;
     }
 
     private function downloadArtefacts(array $artefacts, string $outputDirectory): void
     {
-        $mh = curl_multi_init();
-        $curlHandles = [];
+        $pool = new Pool(
+            $this->circleCiClient,
+            $this->generatePromises($artefacts, $outputDirectory),
+            [
+                'concurrency' => 20,
+                'fulfilled' => function (ResponseInterface $response, int $key) use ($artefacts, $outputDirectory) {
+                },
+                'rejected' => function (RequestException $reason) {
+                    throw new \Exception('Request rejected', previous: $reason);
+                },
+            ],
+        );
 
-        foreach($artefacts as $artefact){
-            $curlHandle = curl_init();
+        $promise = $pool->promise();
+        $promise->wait();
+    }
 
-            curl_setopt_array($curlHandle, array(
-                CURLOPT_URL => $artefact['url'],
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 0,
-                CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_HTTPHEADER => array(
-                    'Content-Type: application/json',
-                    "Circle-Token: $this->token",
-                ),
-            ));
-
-            curl_multi_add_handle($mh, $curlHandle);
-            $curlHandles[] = $curlHandle;
-        }
-
-        $stillRunning = false;
-        do {
-            curl_multi_exec($mh, $stillRunning);
-        } while ($stillRunning);
-
-        $responses = [];
-        foreach($curlHandles as $key => $curlHandle) {
-            curl_multi_remove_handle($mh, $curlHandle);
-            $responses[$key]['content'] = curl_multi_getcontent($curlHandle);
-            $responses[$key]['url'] = $artefacts[$key]['url'];
-            $responses[$key]['http_code'] = curl_getinfo($curlHandle, CURLINFO_HTTP_CODE);
-            curl_close($curlHandle);
-        }
-
-        var_dump(count($curlHandles) . " calls have been made");
-        $notOkRequest = array_filter($responses, fn (array $response) => $response['http_code'] !== 200);
-        if (count($notOkRequest)) {
-            var_dump("Some request are invalid" . json_encode($notOkRequest));
-        }
-
-        curl_multi_close($mh);
-
-        foreach ($responses as $response) {
-            $exploded = explode('/', $response['url']);
-            $outputFile = end($exploded);
-            file_put_contents($outputDirectory . '/' . $outputFile, $response['content']);
+    private function generatePromises(array $artefacts, string $outputDirectory): \Iterator
+    {
+        foreach ($artefacts as $index => $artefact) {
+            yield $index => fn () => $this->circleCiClient->requestAsync('GET', $artefact['url'], [
+                'sink' => $outputDirectory . '/' . $artefact['filename']
+            ]);
         }
     }
 }
